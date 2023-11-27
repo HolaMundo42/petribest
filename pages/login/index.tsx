6120e60591f0a53eeec557a03775665162b0ce62
HolaMundo42 <77760629+HolaMundo42@users.noreply.github.com>
2023-11-27 16:42:39 -0300

    tiny fix deployment

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -54,7 +54,6 @@ export default function Login() {
             </div>
             
             <p className="mt-4 text-sm text-center text-gray-700">
-              Don't have an account?{" "}
               <Link href="/signup" className="font-medium text-blue-600 hover:underline">
                 Sign up
               </Link>
