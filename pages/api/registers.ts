24ed10de3680cc7a474f0b53efb65de601fd5a16
HolaMundo42 <77760629+HolaMundo42@users.noreply.github.com>
2023-11-29 15:02:37 -0300

    fixed back issues

--- pages/api/registers.ts
+++ pages/api/registers.ts
@@ -1,86 +1,92 @@
-// import { PrismaClient, Prisma } from ".prisma/client";
-// import { authOptions } from "../api/auth/[...nextauth]";
-// import { getServerSession } from "next-auth/next";
+import { PrismaClient, Prisma } from ".prisma/client";
+import { authOptions } from "../api/auth/[...nextauth]";
+import { getServerSession } from "next-auth/next";
 
-// const client = new PrismaClient;
+const client = new PrismaClient;
 
 
-// export default async function handler(req:any, res:any){
+export default async function handler(req:any, res:any){
 
-//     console.log(req.body)
+    console.log(req.body)
     
-//     const session = await getServerSession(req, res, authOptions);
+    const session = await getServerSession(req, res, authOptions);
+        const user = await client.user.findFirst ({
+            where: {
+                email: session?.user?.email, //ignorar error
+                }
+        })
     
-//     const user = await client.user.findFirst ({
-//         where: {
-//             email: session.user.email, //ignorar error
-//         }
-//     })
-    
-//     if (req.method === "GET") {
-//         const PetriData = await client.registers.findMany({
-//             where: {
-//                 userId: user.id,
-//             },
-//             select: {
-//                 name: true,
-//                 date: true,
-//                 colonies: true,
-//                 img: true,
-//                 info: true,
-//             },
-//         })
-    
-//         console.log("PetriData: \n", PetriData);
-//         res.status(200).json({data: PetriData});
-//     }
+    if (req.method === "GET") {
+        if (user){
+            const PetriData = await client.registers.findMany({
+                where: {
+                    userId: user.id,
+                },
+                select: {
+                    name: true,
+                    date: true,
+                    colonies: true,
+                    img: true,
+                    info: true,
+                },
+            })
+            
+            console.log("PetriData: \n", PetriData);
+            res.status(200).json({data: PetriData});
+        } 
+    }
 
-//     else if (req.method === "POST") {
-//         console.log("a")
-//         try {
-//             const petriPOST = await client.registers.create({
-//                 data: {
-//                     userId: user.id,
-//                     name: req.body.name as string,
-//                     date: req.body.dato as string,
-//                     colonies: req.body.colonies as number,
-//                     img: req.body.img.url as string,
-//                     info: req.body.info as string,
-//                 },
-//             })    
+    else if (req.method === "POST") {
 
-//             console.log("PetriData petripuesta: \n", petriPOST);
-//             res.status(200).json({
-//                 name: petriPOST.name,
-//                 date: petriPOST.date,
-//                 colonies: petriPOST.colonies,
-//                 img: petriPOST.img,
-//                 info: petriPOST.info,
-//             });
-//         } 
-//         catch (e) {
-//             if (e instanceof Prisma.PrismaClientKnownRequestError) {
-//             if (e.code === 'P2002') {
-//                 console.log(
-//                 'Unique constraint violation'
-//                 )
-//                 res.status(400).json({error: 'P2002'});
-//             }
-//             }
-//         }
-//     }
+        if (user){
+        try {
+            const petriPOST = await client.registers.create({
+                data: {
+                    userId: user.id,
+                    name: req.body.name as string,
+                    date: req.body.dato as string,
+                    colonies: req.body.colonies as number,
+                    img: req.body.img.url as string,
+                    info: req.body.info as string,
+                },
+            })    
+            
+            console.log("PetriData petripuesta: \n", petriPOST);
+            res.status(200).json({
+                name: petriPOST.name,
+                date: petriPOST.date,
+                colonies: petriPOST.colonies,
+                img: petriPOST.img,
+                info: petriPOST.info,
+            });
+        } 
+        catch (e) {
+            if (e instanceof Prisma.PrismaClientKnownRequestError) {
+            if (e.code === 'P2002') {
+                console.log(
+                'Unique constraint violation'
+                )
+                res.status(400).json({error: 'P2002'});
+            }
+            }
+        }
+    }
+    }
 
-//     else if (req.method === "DELETE") {
+    else if (req.method === "DELETE") {
         
-//         const borrarPetriDish = await client.registers.delete ({
-//         where: {
-//             userId: user.id,
-//             name: req.body.name as string,
-//         },
-//         })
+        if (user){
 
-//         console.log("petriBYE (un honor haber sido parte de petrilab): \n", borrarPetriDish);
-//         res.status(200).json({name: borrarPetriDish.name});
-//     }
+            const borrarPetriDish = await client.registers.delete ({
+                where: {
+                    userId: user.id,
+                    name: req.body.name as string,
+                },
+            })
+            
+            console.log("petriBYE (un honor haber sido parte de petrilab): \n", borrarPetriDish);
+            res.status(200).json({name: borrarPetriDish.name});
+        }
+    }
 
-// }
+}
