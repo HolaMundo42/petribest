58f54c321a4cee4b57a550563eca24f7d4d75ae3
HolaMundo42 <77760629+HolaMundo42@users.noreply.github.com>
2023-12-11 13:00:03 -0300

    agua

--- pages/history/index.tsx
+++ pages/history/index.tsx
@@ -40,12 +40,12 @@ const History: React.FC<HistoryProps> = () => {
           setMockData(data.data);
           if (data.data.length > 0) {
             const item = data.data[0];
-            console.log(item)
+            console.log(item.img)
             setSelectedCard(0);
             setSelectedItemName(item.name);
             setSelectedItemDate(item.date);
             setSelectedItemColonies(item.colonies);
-            setSelectedItemImageUrl(item.image);
+            setSelectedItemImageUrl(item.img);
             setSelectedItemInfo(item.info);
           }
         } else {
